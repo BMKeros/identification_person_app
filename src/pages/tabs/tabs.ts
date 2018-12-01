410883fe9cc7bc460c9041989726d54b8047b959
Israel Lugo <hostelixisrael@gmail.com>
2018-12-01 10:25:09 -0400

    [MOD] modified tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { RegistrationPage } from '../registration/registration';
+import { ScannerPage } from '../scanner/scanner';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = ScannerPage;
+  tab2Root = RegistrationPage;
+  tab3Root = HomePage;
 
   constructor() {
 
